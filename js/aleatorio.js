b7a048fcfb384617a8ad52fdda0c6870332c378b
AllanaDallagnol25 <148997157+AllanaDallagnol25@users.noreply.github.com>
2025-07-30 11:31:43 -0300

    merere

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["Sara", "Vinicius", "Adrielly", "Ezequiel", "Otávio", "João", "Caique", "Nathalia"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
